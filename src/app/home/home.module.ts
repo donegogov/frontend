38e55d06c4d19b0a29c47e93d283e2f68766546d
donegogov <donatellorm@gmail.com>
2022-01-09 16:06:00 +0100

    home slider template

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,15 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
 import { SharedModule } from '../shared/shared.module';
+import { HomeSliderComponent } from './home-slider/home-slider.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    HomeSliderComponent,
+    MainContentComponent
   ],
   imports: [
     CommonModule,
